9414165f4dd0167c2cec6616587c0919942c7f44
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-08 08:45:01 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -14,20 +14,16 @@ let titleArea = document.getElementById('titleArea');
 titleArea.style.paddingTop = 350 * widthRatio + 'px';
 
 let array0 = ['Niềm', 'vui', 'tu', 'Đạo',
-    'Chương trình hỏi đáp về<br>Đức Chí Tôn, Đức Ngọc Hoàng Thượng Đế',
+    'Chương trình hỏi đáp về Đức Chí Tôn,',
+    'Đức Ngọc Hoàng Thượng Đế',
     'Bắt đầu'];
 
 for (let i = 0; i < array0.length; i++) {
     let text = document.createElement('p');
     titleArea.appendChild(text);
     text.innerHTML = array0[i];
-    text.style.animation = 'fadeIn 0.5s linear forwards';
-    text.style.animationDelay = 0.5 * (i + 1) + 's';
-
-    if (i === array0.length - 1) {
+    if (i === array0.length-1){
         text.onclick = stopTitleArea;
-        text.style.animation = 'zoomIn 0.5s ease-out alternate infinite';
-        text.style.animationDelay = '3s';
     }
 }
 
@@ -63,10 +59,6 @@ function startIntroArea() {
     introBoard.appendChild(introText);
     introText.id = 'introText';
 
-    tieuDan.style.animation = 'fadeIn 0.7s 0.5s linear forwards,' +
-        'bounce 0.7s 1.2s ease-in alternate infinite';
-    introBoard.style.animation = 'fadeIn 0.5s 1.9s linear forwards';
-
     let array1 = [['Mến chào quý đạo hữu,', 'đệ là <span>Tiểu Dần</span>.'],
         'Mến chúc quý đạo hữu một năm mới',
         'nhiều <span>sức khỏe</span>, thường <span>an lạc</span> và <span>tinh tấn.</span>',
@@ -81,8 +73,6 @@ function startIntroArea() {
         let text1 = document.createElement('p');
         text0.innerHTML = array1[i][0];
         text1.innerHTML = array1[i][1];
-        text0.style.animation = 'fadeIn 0.5s linear forwards';
-        text1.style.animation = 'fadeIn 0.5s 1s linear forwards';
 
         if (i === 0) {
             introText.appendChild(text0);
