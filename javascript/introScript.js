864041528af75aad8bd62bba46177e9885b0ddd6
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-12 09:05:55 +0700

    commit

--- javascript/introScript.js
+++ javascript/introScript.js
@@ -1,38 +1,36 @@
 export {startIntroArea};
 
 let introArea = document.getElementById('introArea');
+
 function startIntroArea() {
     let introBoard = document.createElement('p');
-    introBoard.id = 'introBoard';
+    introBoard.className = 'introBoard';
     introArea.appendChild(introBoard);
 
     let tieuDan = document.createElement('img');
     introArea.appendChild(tieuDan);
-    tieuDan.id = 'tieuDan';
+    tieuDan.className = 'tieuDan';
     tieuDan.src = '../media/TigerFace.png';
 
-    let introText = document.createElement('div');
+    let introText = document.createElement('p');
     introBoard.appendChild(introText);
-    introText.id = 'introText';
+    introText.className = 'introText';
 
-    let array1 = [['Mến chào quý đạo hữu,', 'đệ là <span>Tiểu Dần</span>.'],
-        ['Mến chúc quý đạo hữu một năm mới',
-            'nhiều <span>sức khỏe</span>, thường <span>an lạc</span> và <span>tinh tấn.</span>'],
-        'Nhân dịp Thánh Lễ của<br>Đức Ngọc Hoàng Thượng Đế,',
-        'Tiểu Dần thân mời cả nhà cùng tham gia',
-        'chương trình hỏi đáp Niềm vui tu Đạo',
-        'để cùng nhau tưởng nhớ về Ngài,',
-        'vị Cha Lành từ ái của muôn sinh.'];
+    let array = ['Mến chào quý đạo hữu,<br>đệ là <span>Tiểu Dần</span>.',
+        'Mến chúc quý đạo hữu một năm mới nhiều <span>sức khỏe</span>, thường <span>an lạc</span> và <span>tinh tấn.</span>',
+        'Nhân dịp Thánh Lễ của <span>Đức Ngọc Hoàng Thượng Đế,</span>',
+        '<span>Tiểu Dần</span> thân mời cả nhà cùng tham gia<br>chương trình hỏi đáp <span><br>Niềm vui tu Đạo</span>',
+        'để cùng nhau<br><span>tưởng nhớ</span> về Ngài,',
+        'vị <span>Cha Lành từ ái</span><br>của muôn sinh.'];
 
-    for (let i = 0; i < array1.length; i++) {
-        let text0 = document.createElement('p');
-        let text1 = document.createElement('p');
-        text0.innerHTML = array1[i][0];
-        text1.innerHTML = array1[i][1];
-
-        if (i === 0) {
-            introText.appendChild(text0);
-            introText.appendChild(text1);
-        }
+    let i = 0;
+    introText.innerHTML = array[i];
+    window.onclick = function () {
+        i++;
+        introText.classList.add('remove');
+        setTimeout(function () {
+            introText.innerHTML = array[i];
+            introText.classList.remove('remove');
+        }, 500);
     }
 }
--- javascript/titleScript.js
+++ javascript/titleScript.js
@@ -1,16 +1,16 @@
 import {startIntroArea} from "./introScript.js";
 
 let titleArea = document.getElementById('titleArea');
-let array0 = ['Niềm', 'vui', 'tu', 'Đạo',
+let array = ['Niềm', 'vui', 'tu', 'Đạo',
     'Chương trình hỏi đáp về Đức Chí Tôn,',
     'Đức Ngọc Hoàng Thượng Đế',
     'Bắt đầu'];
-for (let i = 0; i < array0.length; i++) {
+for (let i = 0; i < array.length; i++) {
     let text = document.createElement('p');
     titleArea.appendChild(text);
 
-    text.innerHTML = array0[i];
-    if (i === array0.length - 1) {
+    text.innerHTML = array[i];
+    if (i === array.length - 1) {
         text.onclick = stopTitleArea;
     }
 }
