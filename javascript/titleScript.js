42519a6724418c1f3c39b1300a4988d83e83eeaa
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-12 07:00:01 +0700

    commit

--- javascript/titleScript.js
+++ javascript/titleScript.js
@@ -1,4 +1,4 @@
-import {deAccent} from "./functionScript.js";
+import {startIntroArea} from "./introScript.js";
 
 // alert(screen.width + '/' + screen.height + ','
 //     + outerWidth + '/' + outerHeight);
@@ -27,7 +27,6 @@ if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobil
 }
 
 let titleArea = document.getElementById('titleArea');
-let introArea = document.getElementById('introArea');
 let array0 = ['Niềm', 'vui', 'tu', 'Đạo',
     'Chương trình hỏi đáp về Đức Chí Tôn,',
     'Đức Ngọc Hoàng Thượng Đế',
@@ -54,40 +53,4 @@ function stopTitleArea() {
         document.body.removeChild(titleArea);
         startIntroArea();
     }, 500);
-}
-
-function startIntroArea() {
-    let introBoard = document.createElement('p');
-    introBoard.id = 'introBoard';
-    introArea.appendChild(introBoard);
-
-    let tieuDan = document.createElement('img');
-    introArea.appendChild(tieuDan);
-    tieuDan.id = 'tieuDan';
-    tieuDan.src = '../media/TigerFace.png';
-
-    let introText = document.createElement('div');
-    introBoard.appendChild(introText);
-    introText.id = 'introText';
-
-    let array1 = [['Mến chào quý đạo hữu,', 'đệ là <span>Tiểu Dần</span>.'],
-        ['Mến chúc quý đạo hữu một năm mới',
-            'nhiều <span>sức khỏe</span>, thường <span>an lạc</span> và <span>tinh tấn.</span>'],
-        'Nhân dịp Thánh Lễ của<br>Đức Ngọc Hoàng Thượng Đế,',
-        'Tiểu Dần thân mời cả nhà cùng tham gia',
-        'chương trình hỏi đáp Niềm vui tu Đạo',
-        'để cùng nhau tưởng nhớ về Ngài,',
-        'vị Cha Lành từ ái của muôn sinh.'];
-
-    for (let i = 0; i < array1.length; i++) {
-        let text0 = document.createElement('p');
-        let text1 = document.createElement('p');
-        text0.innerHTML = array1[i][0];
-        text1.innerHTML = array1[i][1];
-
-        if (i === 0) {
-            introText.appendChild(text0);
-            introText.appendChild(text1);
-        }
-    }
 }
