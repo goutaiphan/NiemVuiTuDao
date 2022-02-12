ece9c4286178a1767d0871ae7e8ddba3a193b9f9
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-12 07:14:42 +0700

    commit

---
+++ javascript/baseScript.js
@@ -0,0 +1,28 @@
+// alert(screen.width + '/' + screen.height + ','
+//     + outerWidth + '/' + outerHeight);
+
+let introArea = document.getElementById('introArea');
+if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i)) {
+    let width = Math.min(screen.width, screen.height);
+    let height = Math.max(screen.width, screen.height);
+    let widthRatio = width / 450;
+    let heightRatio = height / 850;
+
+    if (width < 450) {
+        widthRatio = width < 360
+            ? widthRatio
+            : widthRatio
+        document.body.style.width = width + 'px';
+        introArea.style.marginTop = '40px';
+    } else {
+        widthRatio = widthRatio * 0.7;
+        document.body.style.marginTop = width < 1000
+            ? 150 * heightRatio + 'px'
+            : 200 * heightRatio + 'px'
+    }
+    document.body.style.transform = `scale(${widthRatio})`;
+
+} else {
+    document.body.style.marginTop = '60px';
+    introArea.style.marginTop = '40px';
+}
--- javascript/titleScript.js
+++ javascript/titleScript.js
@@ -1,31 +1,5 @@
 import {startIntroArea} from "./introScript.js";
 
-// alert(screen.width + '/' + screen.height + ','
-//     + outerWidth + '/' + outerHeight);
-
-if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i)) {
-    let width = Math.min(screen.width, screen.height);
-    let height = Math.max(screen.width, screen.height);
-    let widthRatio = width / 450;
-    let heightRatio = height / 850;
-
-    if (width < 450) {
-        widthRatio = width < 360
-            ? widthRatio
-            : widthRatio
-        document.body.style.width = width + 'px';
-    } else {
-        widthRatio = widthRatio * 0.7;
-        document.body.style.marginTop = width < 1000
-            ? 150 * heightRatio + 'px'
-            : 200 * heightRatio + 'px'
-    }
-    document.body.style.transform = `scale(${widthRatio})`;
-
-} else {
-    document.body.style.marginTop = '60px';
-}
-
 let titleArea = document.getElementById('titleArea');
 let array0 = ['Niềm', 'vui', 'tu', 'Đạo',
     'Chương trình hỏi đáp về Đức Chí Tôn,',
@@ -34,6 +8,7 @@ let array0 = ['Niềm', 'vui', 'tu', 'Đạo',
 for (let i = 0; i < array0.length; i++) {
     let text = document.createElement('p');
     titleArea.appendChild(text);
+
     text.innerHTML = array0[i];
     if (i === array0.length - 1) {
         text.onclick = stopTitleArea;
