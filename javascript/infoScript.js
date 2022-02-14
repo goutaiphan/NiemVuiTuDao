eda61d46f97587b322d24eaf9bd03dbb07b856fa
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-14 14:53:25 +0700

    commit

--- javascript/infoScript.js
+++ javascript/infoScript.js
@@ -42,87 +42,84 @@ function startInfoArea() {
     introBoard.appendChild(infoButton);
     introBoard.classList.add('resize');
     introArea.style.marginTop = '0';
+}
 
-    userID.onkeydown = function (event) {
-        userID.setCustomValidity('');
-        userID.classList.add('normal');
-        userID.classList.remove('correct', 'incorrect');
-        userPassword.value = '';
-        userPassword.classList.remove('correct', 'incorrect');
-        infoText.innerHTML = array[0];
-        infoButton.innerHTML = 'Đăng nhập/Đăng ký';
-        infoButton.classList.remove('ready');
-        infoButton.classList.add('unready');
-        if (['Enter', 'Return'].includes(event.key)) (userPassword.focus())
-    }
+userID.onkeydown = function (event) {
+    infoText.innerHTML = array[0];
+    infoButton.innerHTML = 'Đăng nhập/Đăng ký';
+    setInfoButton(false);
 
-    userID.onblur = function () {
-        userID.value = deAccent(userID.value);
-        userID.classList.remove('normal');
-        if (userID.value) checkUserID();
-    }
+    userID.setCustomValidity('');
+    userID.classList.remove('correct', 'incorrect');
+    userPassword.value = '';
+    userPassword.classList.remove('correct', 'incorrect');
+    if (['Enter', 'Return'].includes(event.key)) (userPassword.focus())
+}
 
-    userPassword.onkeydown = function (event) {
-        userPassword.setCustomValidity('');
-        userPassword.classList.add('normal');
-        userPassword.classList.remove('correct', 'incorrect');
-        infoButton.classList.remove('ready');
-        infoButton.classList.add('unready');
-        if (['Enter', 'Return'].includes(event.key)) (userPassword.blur());
-    }
+userID.onblur = function () {
+    userID.value = deAccent(userID.value);
+    if (userID.value) checkUserID();
+}
 
-    userPassword.onblur = function () {
-        userPassword.classList.remove('normal');
-        if (userPassword.value) checkUserPassword()
-    }
+userPassword.onkeydown = function (event) {
+    setInfoButton(false);
+    userPassword.setCustomValidity('');
+    userPassword.classList.remove('correct', 'incorrect');
+    if (['Enter', 'Return'].includes(event.key)) (userPassword.blur());
+}
 
-    function checkUserID() {
-        if (userID.value.length < 6 || userID.value.length > 16) {
-            userID.setCustomValidity(array[array.length - 1]);
-            userID.reportValidity();
-        } else {
-            get(child(userRef, userID.value)).then(snapshot => {
-                if (snapshot.val()) {
-                    infoText.innerHTML = array[1];
-                    infoButton.innerHTML = 'Đăng nhập';
-                } else {
-                    infoText.innerHTML = array[2];
-                    infoButton.innerHTML = 'Đăng ký';
-                }
-                userID.classList.add('correct');
-            })
-        }
-    }
+userPassword.onblur = function () {
+    if (userPassword.value) checkUserPassword()
+}
 
-    function checkUserPassword() {
-        if (userPassword.value.length < 6 || userPassword.value.length > 16) {
-            userPassword.setCustomValidity(array[array.length - 1]);
-            userPassword.reportValidity();
-        } else {
-            if (infoButton.innerHTML === 'Đăng nhập') {
+function checkUserID() {
+    if (userID.value.length < 6 || userID.value.length > 16) {
+        userID.setCustomValidity(array[array.length - 1]);
+        userID.reportValidity();
+    } else {
+        get(child(userRef, userID.value)).then(snapshot => {
+            if (snapshot.val()) {
                 infoText.innerHTML = array[1];
-                get(child(userRef, userID.value + '/userPassword')).then(snapshot => {
-                    if (snapshot.val() === null) {
-                    } else if (snapshot.val() !== userPassword.value) {
-                        userPassword.classList.add('incorrect');
-                        userPassword.classList.remove('correct');
-                        navigator.vibrate(500);
-                    } else if (snapshot.val() === userPassword.value) {
-                        userPassword.classList.add('correct');
-                        userPassword.classList.remove('incorrect');
-                        startInfoButton();
-                    }
-                });
+                infoButton.innerHTML = 'Đăng nhập';
             } else {
                 infoText.innerHTML = array[2];
-                userPassword.classList.add('correct');
-                userPassword.classList.remove('incorrect');
-                startInfoButton();
+                infoButton.innerHTML = 'Đăng ký';
             }
+            userID.classList.add('correct');
+        })
+    }
+}
+
+function checkUserPassword() {
+    if (userPassword.value.length < 6 || userPassword.value.length > 16) {
+        userPassword.setCustomValidity(array[array.length - 1]);
+        userPassword.reportValidity();
+    } else {
+        if (infoButton.innerHTML === 'Đăng nhập') {
+            infoText.innerHTML = array[1];
+            get(child(userRef, userID.value + '/userPassword')).then(snapshot => {
+                if (snapshot.val() === null) {
+                } else if (snapshot.val() !== userPassword.value) {
+                    userPassword.classList.add('incorrect');
+                    userPassword.classList.remove('correct');
+                    navigator.vibrate(500);
+                } else if (snapshot.val() === userPassword.value) {
+                    userPassword.classList.add('correct');
+                    userPassword.classList.remove('incorrect');
+                    setInfoButton(true)
+                }
+            });
+        } else {
+            infoText.innerHTML = array[2];
+            userPassword.classList.add('correct');
+            userPassword.classList.remove('incorrect');
+            setInfoButton(true)
         }
     }
+}
 
-    function startInfoButton() {
+function setInfoButton(validity) {
+    if (validity === true) {
         setTimeout(function () {
             infoButton.classList.add('ready');
             infoButton.classList.remove('unready');
@@ -135,60 +132,63 @@ function startInfoArea() {
                     : alert('Đăng ký thành công.')
             }
         }, 200)
+    } else {
+        infoButton.classList.add('unready');
+        infoButton.classList.remove('ready');
+        infoButton.onclick = null;
     }
-
+}
 
 // const app2 = initializeApp({
 //     databaseURL: "https://niemvuitudao1-default-rtdb.asia-southeast1.firebasedatabase.app/"
 // }, 'app2');
 // const scoreDatabase = getDatabase(app2);
 
-    // let userBirthday = document.createElement('div');
-    // introBoard.appendChild(userBirthday);
-    // userBirthday.className = 'userData userBirthday';
-    //
-    // let array = ['dd', 'mm', 'yyyy'];
-    // for (let i = 0; i < array.length; i++) {
-    //     let child = document.createElement('input');
-    //     userBirthday.appendChild(child);
-    //     child.placeholder = array[i];
-    // }
-    //
-    // for (let i = 0; i < 2; i++) {
-    //     let separator = document.createElement('p');
-    //     userBirthday.appendChild(separator);
-    //     separator.innerHTML = '-';
-    // }
-    // userBirthday.onfocus = function () {
-    //     userBirthday.type = 'date';
-    //     userBirthday.required = ''
-    // }
-    // userBirthday.onblur = function () {
-    //     userBirthday.type = 'text';
-    // }
+// let userBirthday = document.createElement('div');
+// introBoard.appendChild(userBirthday);
+// userBirthday.className = 'userData userBirthday';
+//
+// let array = ['dd', 'mm', 'yyyy'];
+// for (let i = 0; i < array.length; i++) {
+//     let child = document.createElement('input');
+//     userBirthday.appendChild(child);
+//     child.placeholder = array[i];
+// }
+//
+// for (let i = 0; i < 2; i++) {
+//     let separator = document.createElement('p');
+//     userBirthday.appendChild(separator);
+//     separator.innerHTML = '-';
+// }
+// userBirthday.onfocus = function () {
+//     userBirthday.type = 'date';
+//     userBirthday.required = ''
+// }
+// userBirthday.onblur = function () {
+//     userBirthday.type = 'text';
+// }
 
-    // userBirthday.onkeydown = function (event) {
-    //     if (event.key.match(/[^\d]/) && !['Tab'].includes(event.key))
-    //         event.preventDefault();
-    //     if (['Clear', 'Backspace', 'Delete'].includes(event.key)) userBirthday.value = '';
-    //     if (['Enter', 'Return', 'OK'].includes(event.key)) (userName.focus())
-    // }
+// userBirthday.onkeydown = function (event) {
+//     if (event.key.match(/[^\d]/) && !['Tab'].includes(event.key))
+//         event.preventDefault();
+//     if (['Clear', 'Backspace', 'Delete'].includes(event.key)) userBirthday.value = '';
+//     if (['Enter', 'Return', 'OK'].includes(event.key)) (userName.focus())
+// }
 
-    // userBirthday.onkeyup = function () {
-    //     let inputValue = this.value;
-    //     if (inputValue.match(/^\d{2}$/) !== null) {
-    //         this.value = inputValue + '-';
-    //     } else if (inputValue.match(/^\d{2}-\d{2}$/) !== null) {
-    //         this.value = inputValue + '-';
-    //     }
-    // }
-    //
-    //
-    // let userName = document.createElement('input');
-    // introBoard.appendChild(userName);
-    // userName.className = 'userData userName';
-    // userName.placeholder = 'Quý danh';
-}
+// userBirthday.onkeyup = function () {
+//     let inputValue = this.value;
+//     if (inputValue.match(/^\d{2}$/) !== null) {
+//         this.value = inputValue + '-';
+//     } else if (inputValue.match(/^\d{2}-\d{2}$/) !== null) {
+//         this.value = inputValue + '-';
+//     }
+// }
+//
+//
+// let userName = document.createElement('input');
+// introBoard.appendChild(userName);
+// userName.className = 'userData userName';
+// userName.placeholder = 'Quý danh';
 
 //     userName.onkeydown = function (event) {
 //         userName.setCustomValidity('');
