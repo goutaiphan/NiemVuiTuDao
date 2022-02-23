a09dd69b317d1c3e6d87d13bdd7d8665e5148b94
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-24 04:04:22 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -27,7 +27,7 @@ function setSizeRatio(object, marginDesktop, marginMobile) {
     }
 
     let padding = (document.body.offsetHeight - object.offsetHeight) / 4;
-    document.body.style.padding = `${padding}px 0`;
+    //document.body.style.padding = `${padding}px 0`;
     object.style.minWidth = 'max-content';
     object.style.transform = `scale(${widthRatio})`;
     // if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i)) {
