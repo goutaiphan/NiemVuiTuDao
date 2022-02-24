4677932c704095d22588728ce7922ebb4dffc48c
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-25 01:26:32 +0700

    commit

--- javascript/baseScript.js
+++ javascript/baseScript.js
@@ -44,7 +44,7 @@ function setSize(object, marginDesktop, marginMobile) {
     document.body.style.padding = padding > 0
         ? `${padding}px 0`
         : '25px 0';
-    console.log(padding);
+    alert(padding);
     if (width < 1080) {
         if (width > 450) widthRatio = widthRatio * 0.7;
         if (height > 800) object.style.marginTop = marginMobile * heightRatio + 'px';
